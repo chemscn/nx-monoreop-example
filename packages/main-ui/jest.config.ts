003bfbe88bec4589019695aeb202df7e99389d8e
Diyar Karim <DKARIM1@ford.com>
2022-06-06 18:15:22 -0400

    first commit

---
+++ packages/main-ui/jest.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'main-ui',
+  preset: '../../jest.preset.ts',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/main-ui',
+};
---
+++ packages/secondary-ui/jest.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'secondary-ui',
+  preset: '../../jest.preset.ts',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/secondary-ui',
+};
